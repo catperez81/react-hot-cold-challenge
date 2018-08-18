ef38dd0e060accbe155afa31c05fb7f01b54f507
catperez81 <catperez81@gmail.com>
2018-08-18 08:18:16 -0700

    first test passing - yay

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -1,4 +1,6 @@
-import { shallow } from 'enzyme';
+import React from 'react';
+import {shallow, mount, render} from 'enzyme';
+import TopNav from './top-nav';
 
 it('Renders without crashing', () => {
     shallow(<TopNav />);
