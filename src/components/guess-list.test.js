fed895c92ce4d8dc8a2682068dce240740c7173a
catperez81 <catperez81@gmail.com>
2018-08-21 08:57:23 -0700

    guess list test working

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -2,6 +2,20 @@ import React from 'react';
 import {shallow, mount, render} from 'enzyme';
 import GuessList from './guess-list';
 
-it('Renders without crashing', () => {
-    shallow(<GuessList guesses={[]}/>);
+describe('<GuessList />', () => {
+	it('Renders without crashing', () => {
+	    shallow(<GuessList guesses={[]}/>);
+	});
+
+	it('Renders the guesses', () => {
+		const values = [2, 45, 29];
+		const wrapper = shallow(<GuessList guesses={values} />);
+		const items = wrapper.find('li');
+		// below we find the list items //
+		expect(items.length).toEqual(values.length);
+		// go through each list item and try to see if they match. toString converts to a big string. //
+		values.forEach((value, index) => {
+			expect(items.at(index).text()).toEqual(value.toString());
+		});
+	});
 });
